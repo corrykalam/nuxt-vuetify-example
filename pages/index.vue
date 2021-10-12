120ea2605c640247dce5c7fe1c0df20fd05216aa
corrykalam <corryf0307@gmail.com>
2021-10-12 23:18:00 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-row>
+    <v-col cols="8" class="frame-content px-6">
+      <products></products>
+    </v-col>
+    <v-col cols="4" class="frame-content white">
+      ORDER
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.frame-content {
+  height: 90vh;
+  overflow-y: auto;
+}
+</style>
